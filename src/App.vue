567802239f83d8d22180a69fce31d7b3baa8a215
郑雲城 <8606606+zheng-yuncheng@user.noreply.gitee.com>
2021-10-24 21:58:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    父组件：{{obj}}
+    <Son :obj="obj"/>
+  </div>
+
+</template>
+
+<script>
+import Son from './Son.vue'
+export default {
+  name: 'App',
+  components: {
+    Son,
+  },
+  data() {
+    return {
+      obj: 12
+    }
+  }
+}
+</script>
---
+++ src/Son.vue
@@ -0,0 +1,16 @@
+<template>
+    <div @click="btn">
+        子组件:{{obj}}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['obj'],
+        methods: {
+            btn() {
+                this.obj = 33
+            }
+        },
+    }
+</script>
